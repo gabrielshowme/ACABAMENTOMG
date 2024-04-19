484e3fb98fa4fbc3be5c8c3b5073d2c4b1e74bae
gabrielshowme <86712827+gabrielshowme@users.noreply.github.com>
2024-04-19 20:25:23 -0300

    Primeiro Commit

---
+++ app/Http/Controllers/pg/controller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\pg;
+
+
+class controller 
+{
+    //Tela de login
+    public function inicio(){
+        return view('login');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\pg\controller AS Pgcontroller;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [Pgcontroller::class, 'inicio']);
+
+// Pagina em branco
+Route::get('felipe', function () { return view('meuestudo'); });
+
+Route::get('profile', function () { return view('profile'); });
